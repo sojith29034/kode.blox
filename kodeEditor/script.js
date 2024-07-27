c0be84b4c4da489a3c4773eb00110d694779b5b6
Sojith Sunny <sojithsunny29034@gmail.com>
2024-07-27 19:51:04 +0530

    reload confirmation added

--- kodeEditor/script.js
+++ kodeEditor/script.js
@@ -5,14 +5,25 @@ function run() {
     let outputLaptop = document.querySelector(".output #output");
     let outputMobile = document.querySelector(".sidebar .preview #preview");
 
-    outputLaptop.contentDocument.body.innerHTML = html + "<style>"+css+"</style>";
+    outputLaptop.contentDocument.body.innerHTML = html + "<style>" + css + "</style>";
     outputLaptop.contentWindow.eval(js);
 
-    outputMobile.contentDocument.body.innerHTML = html + "<style>"+css+"</style>";
+    outputMobile.contentDocument.body.innerHTML = html + "<style>" + css + "</style>";
     outputMobile.contentWindow.eval(js);
 }
 
+document.querySelector(".editor #html").addEventListener("keyup", run);
+document.querySelector(".editor #css").addEventListener("keyup", run);
+document.querySelector(".editor #js").addEventListener("keyup", run);
 
-document.querySelector(".editor #html").addEventListener("keyup",run);
-document.querySelector(".editor #css").addEventListener("keyup",run);
-document.querySelector(".editor #js").addEventListener("keyup",run);
+window.addEventListener("beforeunload", function (e) {
+    let html = document.querySelector(".editor #html").value;
+    let css = document.querySelector(".editor #css").value;
+    let js = document.querySelector(".editor #js").value;
+
+    if (html || css || js) {
+        const warningMessage = "You have unsaved changes. Are you sure you want to leave?";
+        e.preventDefault();
+        return warningMessage
+    }
+});
